fca8ff81d90910d72ef856449405393556f6f125
Ivan Echegaray Avendaño <admin@localhost.localdomain>
2019-05-18 19:58:37 -0500

    init

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,18 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_gcc_server_is_installed(host):
+    gcc = host.package('gcc')
+
+    assert gcc.is_installed
+
+def test_passwd_file(host):
+    passwd = host.file("/etc/passwd")
+    assert passwd.contains("root")
+    assert passwd.user == "root"
+    assert passwd.group == "root"
